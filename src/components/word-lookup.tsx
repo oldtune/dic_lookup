84d1be88f64f893088a75730307f91ff927c0746
QuackQuack <suddorun@gmail.com>
2023-07-24 23:42:06 +0700

    add route and configure backend

--- src/components/word-lookup.tsx
+++ src/components/word-lookup.tsx
@@ -3,7 +3,7 @@ import { useEffect, useRef, useState } from "react";
 import "../styles/word-lookup.css";
 
 export type WordLookupProps = {
-  word?: string;
+  word: string;
 };
 
 export const WordLookup: React.FC<WordLookupProps> = (props: WordLookupProps) => {
@@ -16,26 +16,27 @@ export const WordLookup: React.FC<WordLookupProps> = (props: WordLookupProps) =>
     }
   })
 
-  // const keyUp = (key: string) => {
-  //   console.log(key);
-  //   if (key === "Enter") {
-  //     console.log('enter pressed');
-  //   }
-  // }
-
-  // const keyUpTest = (key: React.KeyboardEvent<HTMLInputElement>) => {
-  //   console.log(key);
-  // }
-
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log('submitted');
+    navigateIfNotEmpty(inputValue);
+  }
+
+  const navigateToWord = (word: string) => {
+    window.location.href = `/word/${word}`;
+  }
+
+  const navigateIfNotEmpty = (word: string) => {
+    if (word == "") {
+      return;
+    }
+
+    navigateToWord(word);
   }
 
   return <div className="relative word-lookup-wrapper">
     <form ref={formRef} onSubmit={(event) => onSubmit(event)}>
       <input placeholder="Search here..." onChange={(event) => setInputValue(event.target.value)} className="w-full h-10" />
-      <i className="fa-solid fa-magnifying-glass search-icon absolute cursor-pointer" title="Perform search" onClick={() => { console.log('submitted') }}></i>
+      <i className="fa-solid fa-magnifying-glass search-icon absolute cursor-pointer" title="Perform search" onClick={() => navigateIfNotEmpty(inputValue)}></i>
     </form>
   </div>;
 };
