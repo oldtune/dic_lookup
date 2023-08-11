5987f0626f0ce94e6e2f3f2d879c3fb200c4680e
Do Tran <do.tran@saigontechnology.com>
2023-08-11 16:25:56 +0700

    feat: :sparkles: Add some code for suggestion in layout

--- src/components/word-lookup.tsx
+++ src/components/word-lookup.tsx
@@ -9,6 +9,7 @@ export type WordLookupProps = {
 export const WordLookup: React.FC<WordLookupProps> = (props: WordLookupProps) => {
   const [inputValue, setInputValue] = useState(props.word);
   const formRef = useRef<any>();
+  const suggestionDiaglogRef = useRef<any>();
 
   useEffect(() => {
     if (props.word) {
@@ -33,10 +34,20 @@ export const WordLookup: React.FC<WordLookupProps> = (props: WordLookupProps) =>
     navigateToWord(word);
   }
 
-  return <div className="relative word-lookup-wrapper">
-    <form ref={formRef} onSubmit={(event) => onSubmit(event)}>
-      <input placeholder="Search here..." onChange={(event) => setInputValue(event.target.value)} className="w-full h-10" />
-      <i className="fa-solid fa-magnifying-glass search-icon absolute cursor-pointer" title="Perform search" onClick={() => navigateIfNotEmpty(inputValue)}></i>
-    </form>
-  </div>;
+  return (<div className="relative overflow-visible">
+    <div className="relative word-lookup-wrapper">
+      <form ref={formRef} onSubmit={(event) => onSubmit(event)}>
+        <input placeholder="Search here..." onChange={(event) => setInputValue(event.target.value)} className="w-full h-10" />
+        <i className="fa-solid fa-magnifying-glass search-icon absolute cursor-pointer" title="Perform search" onClick={() => navigateIfNotEmpty(inputValue)}></i>
+      </form>
+    </div>
+    <div ref={suggestionDiaglogRef} className="cursor-pointer word-suggestion border-2 border-black border-solid absolute top-12 left-0 w-full">
+      <div className="border-l-8 selected">Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+    </div>
+  </div>
+  );
 };
